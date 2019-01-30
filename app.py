4c916714431a4566421d53fd39fe9b19c988e3e4
pepitooo <Michel.Carteaux@gmail.com>
2019-01-30 16:18:49 +0100

    add overflow time on period summary

--- app.py
+++ app.py
@@ -33,6 +33,9 @@ def get_events():
             AND(Scan.q.timestamp>start_dt.float_timestamp, Scan.q.timestamp<end_dt.float_timestamp)
         ).orderBy('timestamp')
 
+    if active_scans.count() == 0:
+        return jsonify({})
+
     periods = []
     current_period = {}
     last_active_scan_timestamp = None
@@ -69,11 +72,14 @@ def get_events():
 
     daily_no_break_summary = [{'start': k, 'start_ts': min(values), 'end_ts': max(values)} for k, values in pivot_no_break.items()]
 
+    final_summary_duration = pendulum.duration(minutes=0)
     for item in daily_summary:
         duration = pendulum.duration(minutes=item['total_duration'])
         item['title'] = "Daily total : {hours:02}:{minutes:02}".format(hours=duration.hours, minutes=duration.minutes),
         item['color'] = '#257e4a'
+        final_summary_duration += duration
 
+    final_no_break_summary_duration = pendulum.duration(minutes=0)
     for item in daily_no_break_summary:
         time_period = pendulum.from_timestamp(item['end_ts']) - pendulum.from_timestamp(item['start_ts'])
         duration = pendulum.duration(minutes=time_period.in_minutes())
@@ -81,15 +87,32 @@ def get_events():
         item['color'] = '#FF6600'
         del item['start_ts']
         del item['end_ts']
+        final_no_break_summary_duration += duration
 
     for period in periods:
         time_period = pendulum.from_timestamp(period['end']) - pendulum.from_timestamp(period['start'])
         duration = pendulum.duration(minutes=time_period.in_minutes())
-        period['title'] = "Total : {hours:02}:{minutes:02}".format(hours=duration.hours, minutes=duration.minutes)
+        period['title'] = "{hours:02}:{minutes:02}".format(hours=duration.hours, minutes=duration.minutes)
         period['start'] = pendulum.from_timestamp(period['start']).isoformat()
         period['end'] = pendulum.from_timestamp(period['end']).isoformat()
 
-    return jsonify(periods + daily_summary + daily_no_break_summary)
+    final_summary = [
+        {
+            'start': end_dt.subtract(days=1).to_date_string(),
+            'title': ' Total : {hours:02}:{minutes:02}'.format(
+                hours=final_summary_duration.hours + final_summary_duration.days * 24,
+                minutes=final_summary_duration.minutes),
+            'color': '#FF0000'
+        },
+        {
+            'start': end_dt.subtract(days=1).to_date_string(),
+            'title': 'No beak : {hours:02}:{minutes:02}'.format(
+                hours=final_no_break_summary_duration.hours + final_no_break_summary_duration.days * 24,
+                minutes=final_no_break_summary_duration.minutes),
+            'color': '#FF0000'
+        }
+    ]
+    return jsonify(periods + daily_summary + daily_no_break_summary + final_summary)
 
 
 if __name__ == '__main__':
