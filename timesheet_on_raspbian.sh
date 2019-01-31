8f9942620a0db1f8bad25e85f3d5dd37eebb2e6e
pepitooo <michel.carteaux@gmail.com>
2019-01-31 15:08:40 +0100

    small script to speed up installation

--- timesheet_on_raspbian.sh
+++ timesheet_on_raspbian.sh
@@ -14,13 +14,7 @@ pip install -r requirements.txt
 cd static
 bower install
  
-
-cd /etc/supervisor/conf.d/
-sudo ln -s /home/pi/timesheet/config_files/supervisor timesheet.conf
-cd /etc/nginx/sites-available
-sudo ln -s /home/pi/timesheet/config_files/config_files/nginx timesheet
-sudo ln -s timesheet ../sites-enabled
-sudo rm /etc/nginx/sites-enabled/default
-
+sudo cp /home/pi/timesheet/config_files/supervisor timesheet.conf
+sudo cp /home/pi/timesheet/config_files/nginx /etc/nginx/nginx.conf
 sudo cp /home/pi/timesheet/config_files/cron /etc/cron.d/timesheet
 sudo chmod +x /etc/cron.d/timesheet
