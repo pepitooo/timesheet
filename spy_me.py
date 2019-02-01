5e1727d5d1ab618caf2e92e21c96a33c15e55e85
pepitooo <Michel.Carteaux@gmail.com>
2019-02-01 23:03:44 +0100

    use celery

--- spy_me.py
+++ spy_me.py
@@ -1,11 +1,23 @@
 import argparse
+import configparser
 import logging
 import logging.config
+import os
 import sys
 import subprocess
 import time
 
+from celery import Celery
+
 from model import Device, Scan
+
+app = Celery(
+    'tirs_celery',
+    broker='redis://localhost:6379/0',
+    # ## add result backend here if needed.
+    backend='redis://localhost:6379/0'
+)
+app.conf.timezone = 'Europe/Zurich'
 
 
 LOGGING = {
@@ -34,6 +46,30 @@ LOGGING = {
 }
 
 
+@app.on_after_configure.connect()
+def setup_periodic_tasks(sender, **kwargs):
+    config = configparser.ConfigParser()
+    config['default'] = {
+        'bluetooth_mac_address': 'xx:xx:xx:xx:xx'
+    }
+    config.read(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'timesheet.conf'))
+    bluetooth_mac_address = config['default']['bluetooth_mac_address']
+    sender.add_periodic_task(1.00*60, check_bluetooth_presence.s(bluetooth_mac_address))
+
+
+@app.task
+def check_bluetooth_presence(mac_address):
+    bluetooth_ping_cmd = ['l2ping', mac_address, '-c', '1']
+    ret = subprocess.call(bluetooth_ping_cmd, shell=False)
+
+    devices = Device.selectBy(mac_address=mac_address)
+    if devices.count():
+        device = devices.getOne()
+    else:
+        device = Device(mac_address=mac_address)
+    Scan(timestamp=float(time.time()), device=device, present=not ret)
+
+
 def define_logging_conf():
     logging.config.dictConfig(LOGGING)
 
@@ -58,15 +94,7 @@ def main(args):
     args_parsed = parse_args(args)
     mac_address = args_parsed.mac_address
 
-    bluetooth_ping_cmd = ['l2ping', mac_address, '-c', '1']
-    ret = subprocess.call(bluetooth_ping_cmd, shell=False)
-
-    devices = Device.selectBy(mac_address=mac_address)
-    if devices.count():
-        device = devices.getOne()
-    else:
-        device = Device(mac_address=mac_address)
-    Scan(timestamp=float(time.time()), device=device, present=not ret)
+    check_bluetooth_presence(mac_address)
 
 
 if __name__ == '__main__':
--- timesheet_on_raspbian.sh
+++ timesheet_on_raspbian.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y supervisor nginx python3 sqlite3 virtualenv git npm
+sudo apt install -y supervisor nginx python3 sqlite3 virtualenv git npm redis-server
 sudo npm install -g bower
 
 cd
@@ -13,11 +13,14 @@ virtualenv -p python3 .
 pip install -r requirements.txt 
 cd static
 bower install
- 
+
+
 sudo cp /home/pi/timesheet/config_files/supervisor /etc/supervisor/conf.d/timesheet.conf
 sudo cp /home/pi/timesheet/config_files/nginx /etc/nginx/nginx.conf
-sudo cp /home/pi/timesheet/config_files/cron /etc/cron.d/timesheet
-sudo chmod +x /etc/cron.d/timesheet
+
+# cron is maybe not the best idea as it every minutes
+#sudo cp /home/pi/timesheet/config_files/cron /etc/cron.d/timesheet
+#sudo chmod +x /etc/cron.d/timesheet
 
 sudo service nginx restart
 sudo service supervisor restart
